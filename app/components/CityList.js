59a79123e12cdccf54520c9f118efff97bfaa30e
Florian Genaudet <fgenaudet@pc132.home>
2016-04-05 14:44:00 +0200

    Starting find by cities

--- app/components/CityList.js
+++ app/components/CityList.js
@@ -1,10 +1,65 @@
-import React, {Component, StyleSheet, View, Image, Text, Dimensions, TouchableOpacity} from 'react-native';
-import {Actions} from 'react-native-router-flux'
+import React,
+    {Component, StyleSheet, View, ScrollView, Text,
+    TextInput, Dimensions, TouchableOpacity} from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+//App
+import MapService from '../services/MapService';
+
+const styles = StyleSheet.create({
+   page: {
+       paddingTop: 65
+   },
+    separator: {
+        height: 1,
+        backgroundColor: 'black',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10
+    }
+});
 
 export default class CityList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showLoader: true,
+            cities: []
+        }
+    }
+    componentWillMount() {
+        MapService.getAllCity().then(res => {
+           let cities = res.features.map(c => {
+               return {name: c.attributes.COMMUNE};
+           }).sort((a,b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1) ;
+           this.setState({showLoader: false, cities})
+        });
+    }
     render() {
+        let cities = this.state.cities;
+        if(this.state.filter) {
+            cities = cities.filter(c => c.name.toLowerCase().startsWith(this.state.filter.toLowerCase()));
+        }
         return (
-            <Text>City List</Text>
+            <ScrollView style={styles.page}>
+                <Spinner visible={this.state.showLoader} color="red" />
+                <TextInput
+                    style={{height: 40, borderBottomColor: 'gray', borderBottomWidth: 1, marginBottom: 10, padding: 5}}
+                    onChangeText={(filter) => this.setState({filter})}
+                    value={this.state.filter}
+                    placeholder="Search a city"
+                />
+                <View style={styles.separator} />
+                {
+                    cities.map((c,i) => {
+                        return (
+                            <View key={i}>
+                                <Text>{c.name.toUpperCase()}</Text>
+                            </View>
+                        )
+                    })
+                }
+            </ScrollView>
         )
     }
 }
--- app/services/MapService.js
+++ app/services/MapService.js
@@ -1,6 +1,7 @@
 export default class MapService {
     static URL = 'https://ge.ch/sitgags1/rest/services/VECTOR/SITG_OPENDATA_04/MapServer/6592/query?where=@{where}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=SPORT%2C+TYPE%2C+COMMUNE%2C+NCOM%2C+LIEN_FICHE_DESCRIPTIVE%2C+LIEN_PHOTOS&returnGeometry=true&returnTrueCurves=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson';
     static URL_SPORTS = 'https://ge.ch/sitgags1/rest/services/VECTOR/SITG_OPENDATA_04/MapServer/6592/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=SPORT&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=true&resultOffset=&resultRecordCount=&f=pjson';
+    static URL_CITIES = 'https://ge.ch/sitgags1/rest/services/VECTOR/SITG_OPENDATA_04/MapServer/6592/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=COMMUNE&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=true&resultOffset=&resultRecordCount=&f=pjson';
     static URL_TRANSFORM = 'http://geodesy.geo.admin.ch/reframe/lv95towgs84';
 
     static getAll() {
@@ -13,6 +14,9 @@ export default class MapService {
     }
     static getAllSport() {
         return fetch(this.URL_SPORTS).then(res => res.json());
+    }
+    static getAllCity() {
+        return fetch(this.URL_CITIES).then(res => res.json());
     }
     static transform(point) {
         return fetch(this.URL_TRANSFORM+`?easting=${point.x}&northing=${point.y}&format=json`)
